41492192f0ea7a436251f4936df3750f61ed8faa
Mattia Roccoberton <mroccoberton@mbpdev004.local>
2017-02-04 00:14:18 +0100

    Added button to hide/show menu

--- vendor/assets/javascripts/rails_admin/themes/material/ui.js
+++ vendor/assets/javascripts/rails_admin/themes/material/ui.js
@@ -1,6 +1,33 @@
 //= require rails_admin/themes/material/bootstrap-material-design/material
 //= require rails_admin/themes/material/bootstrap-material-design/ripples
 
+var classes_sidebar = false;
+var classes_content = false;
+
+function toggleMenu( e ) {
+  e.preventDefault();
+  if( classes_sidebar ) {
+    $('.sidebar-nav > .nav').animate( { width: 'toggle' }, 200 );
+    $('.sidebar-nav').attr( 'class', classes_sidebar )
+    $('.sidebar-nav ~ div').attr( 'class', classes_content );
+    classes_sidebar = false;
+    $('#btn-menu-wrapper > a').addClass( 'opened' );
+    $('#btn-menu-wrapper i').attr( 'class', 'icon-chevron-left' );
+  }
+  else {
+    $('.sidebar-nav > .nav').animate( { width: 'toggle' }, 200 );
+    classes_sidebar = $('.sidebar-nav').attr( 'class' );
+    classes_content = $('.sidebar-nav ~ div').attr( 'class' );
+    $('.sidebar-nav').attr( 'class', 'sidebar-nav' );
+    $('.sidebar-nav ~ div').attr( 'class', 'content-expanded' );
+    $('#btn-menu-wrapper > a').removeClass( 'opened' );
+    $('#btn-menu-wrapper i').attr( 'class', 'icon-chevron-right' );
+  }
+}
+
 $(document).on('rails_admin.dom_ready', function(){
   $.material.init();
+  if( !$('#btn-menu-wrapper').length ) {
+    $('.sidebar-nav').prepend( '<div id="btn-menu-wrapper"><a href="#" class="btn btn-info btn-xs opened" onclick="Javascript:toggleMenu(event)"><i class="icon-chevron-left"></i></a></div>' );
+  }
 });
